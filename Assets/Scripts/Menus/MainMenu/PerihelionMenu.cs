2dfbf4ef44eb7a28e54002b88f7883e2876014d3
AlbertGimenezMasclans <150661196+AlbertGimenezMasclans@users.noreply.github.com>
2025-05-10 22:09:10 +0200

    Revert "Merge branch 'US25.6_SaveSystem' into US25_Capitulo1_Aprender"
    This reverts commit 351467ed3930213599d1e5edc670f5c2147ae10c, reversing
    changes made to 0141e9cc69bb6ce7e5f239460d8bdc77833557c8.

--- Assets/Scripts/Menus/MainMenu/PerihelionMenu.cs
+++ Assets/Scripts/Menus/MainMenu/PerihelionMenu.cs
@@ -68,9 +68,6 @@ public class PerihelionMenu : MonoBehaviour
             return;
         }
 
-        // Verificar si hay una partida guardada
-        gameLoaded = SaveSystem.HasSavedGame() || PlayerPrefs.GetInt("GameLoaded", 0) == 1;
-
         // Configurar el slider de Música (BGM)
         if (musicSlider != null)
         {
@@ -318,7 +315,6 @@ public class PerihelionMenu : MonoBehaviour
         if (currentOption == MenuOption.StartGame)
         {
             PlaySelectionSound();
-            SaveSystem.SaveDefaultGame(); // Sobrescribir JSON con datos por defecto
             yield return StartCoroutine(FadeIn(0.80f));
             SceneManager.LoadScene("Perihelion");
         }
